e60b9a4f96f9bb98f13b6c190a61f7ed881ebbf8
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-01-29 18:19:12 -0800

    added notes to cryptoId.js

--- src/utilities/cryptoId.js
+++ src/utilities/cryptoId.js
@@ -3,6 +3,7 @@
 const crypto = require('crypto');
 const dayjs = require('dayjs');
 
+// Creates a unique hash code for IDs using Crypto and Dayjs
 module.exports = () => crypto.createHash('md5')
                             .update(
                                 dayjs().format('YYYY-MM-DD[T]HH:mm:ss.SSS')
